65e1cfc80f8f0c93d96c33c201f71bbb7257b4e5
Jordan Davis <jordan.davis@metova.com>
2016-02-04 16:55:34 -0600

    Store fetched ASK request data in nested :ask_request map.

--- lib/alexa_plug/plugs.ex
+++ lib/alexa_plug/plugs.ex
@@ -1,8 +1,8 @@
 defmodule AlexaPlug.Plugs do
   @moduledoc """
-  A collection of plugs for extracting data from Amazon Echo requests.
+  A collection of plugs for extracting data from Alexa Skills Kit requests.
 
-  The plugs fetch specific data from Amazon Echo requests and places
+  The plugs fetch specific data from Alexa Skills Kit requests and places
   that data in `conn.assigns`.
 
   ## Usage
@@ -22,6 +22,9 @@ defmodule AlexaPlug.Plugs do
               "weekly": false,
               "monthly": false
             }
+          },
+          "user": {
+            "userId": "amzn1.account.AM3B00000000000000000000000"
           },
           ...
         },
@@ -43,6 +46,7 @@ defmodule AlexaPlug.Plugs do
   and the following plugs are being used:
 
       plug :fetch_echo_session_attributes
+      plug :fetch_echo_user_id
       plug :fetch_echo_request_type
       plug :fetch_echo_intent_type
       plug :fetch_echo_slots
@@ -54,16 +58,19 @@ defmodule AlexaPlug.Plugs do
   then `conn.assigns` will contain:
 
       %{
-        session_attributes: %{
-          "supportedHoroscopePeriods" => %{
-            "daily" => true,
-            "weekly" => false,
-            "monthly" => false
-          }
-        },
-        request_type: "IntentRequest",
-        intent_type: "GetZodiacHoroscopeIntent",
-        slots: %{"ZodiacSign" => "virgo"}
+        ask_request: %{
+          session_attributes: %{
+            "supportedHoroscopePeriods" => %{
+              "daily" => true,
+              "weekly" => false,
+              "monthly" => false
+            }
+          },
+          user_id: "amzn1.account.AM3B00000000000000000000000",
+          request_type: "IntentRequest",
+          intent_type: "GetZodiacHoroscopeIntent",
+          slots: %{"ZodiacSign" => "virgo"}
+        }
       }
 
   To see more info on all request types see the
@@ -74,37 +81,51 @@ defmodule AlexaPlug.Plugs do
   import Plug.Conn
 
   @doc """
-  Fetch the session attributes from an Amazon Echo request.
+  Fetch the session attributes from an Alexa Skills Kit request.
 
-  Assigns `conn.assigns` the value for the request field `session.attributes` or nil.
+  Assigns `conn.assigns[:ask_request][:session_attributes]` the value
+  for the request field `session.attributes` or nil.
   """
   def fetch_echo_session_attributes(conn, _opts \\ nil) do
     session_attributes = get_in(conn.params, ["session", "attributes"])
-    assign(conn, :session_attributes, session_attributes)
+    assign_to_ask_request(conn, :session_attributes, session_attributes)
+  end
+
+  @docs """
+  Fetch the user ID from an Alexa Skills Kit request.
+
+  Assigns `conn.assigns[:ask_request][:user_id]` the value for the
+  request field `session.user.userId` or nil.
+  """
+  def fetch_echo_user_id(conn, _opts \\ nil) do
+    user_id = get_in(conn.params, ["session", "user", "userId"])
+    assign_to_ask_request(conn, :user_id, user_id)
   end
 
   @doc """
-  Fetch the request type from an Amazon Echo request.
+  Fetch the request type from an Alexa Skills Kit request.
 
-  Assigns `conn.assigns` the value for the request field `request.type` or nil.
+  Assigns `conn.assigns[:ask_request][:request_type]` the value for
+  the request field `request.type` or nil.
   """
   def fetch_echo_request_type(conn, _opts \\ nil) do
     request_type = get_in(conn.params, ["request", "type"])
-    assign(conn, :request_type, request_type)
+    assign_to_ask_request(conn, :request_type, request_type)
   end
 
   @doc """
-  Fetch the intent type from an Amazon Echo request.
+  Fetch the intent type from an Alexa Skills Kit request.
 
-  Assigns `conn.assigns` the value for the request field `request.intent.name` or nil.
+  Assigns `conn.assigns[:ask_request][:intent_type]` the value for
+  the request field `request.intent.name` or nil.
   """
   def fetch_echo_intent_type(conn, _opts \\ nil) do
     intent_type = get_in(conn.params, ["request", "intent", "name"])
-    assign(conn, :intent_type, intent_type)
+    assign_to_ask_request(conn, :intent_type, intent_type)
   end
 
   @doc """
-  Fetch the slots from an Amazon Echo request.
+  Fetch the slots from an Alexa Skills Kit request.
 
   For the given slots:
 
@@ -115,9 +136,11 @@ defmodule AlexaPlug.Plugs do
         }
       }
 
-  A slots map is created using the **slot key** (not the slot name) and the **slot value** as key-value pairs.
+  A slots map is created using the **slot key** (not the slot name)
+  and the **slot value** as key-value pairs.
 
-  Assigns `conn.assigns` the value for the request field `request.intent.slots` or nil.
+  Assigns `conn.assigns[:ask_request][:slots]` the value for the
+  request field `request.intent.slots` or nil.
   """
   def fetch_echo_slots(conn, _opts \\ nil) do
     slots = get_in(conn.params, ["request", "intent", "slots"])
@@ -127,23 +150,36 @@ defmodule AlexaPlug.Plugs do
         Map.put(acc, slot_name, slot_object["value"])
       end)
 
-      assign(conn, :slots, mapped_slots)
+      assign_to_ask_request(conn, :slots, mapped_slots)
     else
-      assign(conn, :slots, nil)
+      assign_to_ask_request(conn, :slots, nil)
     end
   end
 
   @doc """
   Fetch the session attributes, request type, intent type, and slots
-  from an Amazon Echo request.
+  from an Alexa Skills Kit request.
 
   Calls all the other plugs on defined in this module.
   """
   def fetch_echo_request_data(conn, _opts \\ nil) do
     conn
     |> fetch_echo_session_attributes
+    |> fetch_echo_user_id
     |> fetch_echo_request_type
     |> fetch_echo_intent_type
     |> fetch_echo_slots
+  end
+
+  # helper function for assigning fetched request attributes
+  # to the nested :ask_request map
+  defp assign_to_ask_request(conn, key, value) do
+    ask_request_map = Map.get(conn.assigns, :ask_request)
+
+    if ask_request_map do
+      assign(conn, :ask_request, Map.put(ask_request_map, key, value))
+    else
+      assign(conn, :ask_request, Map.put(%{}, key, value))
+    end
   end
 end
